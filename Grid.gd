47ef33627d59b08724d088a1522a12b0eff4db79
Mike Dockerty <cardinal23@gmail.com>
2022-01-12 08:27:25 -0600

    Add Hex highlighting and buggy pixel to hex functions

--- Grid.gd
+++ Grid.gd
@@ -5,7 +5,9 @@ var Hex = preload("res://Hex.tscn")
 var hexes = {}
 
 var hexSize = 160
-var gridSize = Vector2(15,8)
+var gridSize = Vector2(5,5)
+
+var highlightedHex: Vector2
 
 func _init():
     var size = hexSize / 2
@@ -25,26 +27,79 @@ func _init():
             add_child(hex)
             var coordinates = Vector2(x,y)
             hex.coordinates = coordinates
+            hex.axialCoordinates = oddq_to_axial(coordinates)
             hexes[coordinates] = hex
             
 func _input(event):
     if event is InputEventMouseMotion:
-        print(event.position)
+        var eventCoordinates = pixel_to_flat_hex(event.position)
+        highlightHex(eventCoordinates)
+        print(eventCoordinates)
+        
+func highlightHex(coordinates):
+    if !hexes.has(coordinates) || coordinates == highlightedHex:
+        return 
+     
+    if highlightedHex != null:
+        var hex = hexes[highlightedHex]
+        hex.isHighlighted = false
+        hex.update()
+           
+    var hex = hexes[coordinates]
+    highlightedHex = coordinates
+    hex.isHighlighted = true
+    hex.update()
         
 # Hex grid functions
 
 # https://www.redblobgames.com/grids/hexagons/#coordinates-axial
-func oddqToAxial(coordinates):
+func pixel_to_flat_hex(point):
+    var q = ( float(2)/3 * point.x                        ) / hexSize
+    var r = (float(-1)/3 * point.x  +  sqrt(3)/3 * point.y) / hexSize
+    return axial_to_oddq(axial_round(Vector2(q, r)))
+
+func oddq_to_axial(coordinates):
     var q = coordinates.x
-    var r = coordinates.y - (coordinates.y - (coordinates.y&1)) / 2
+    var r = coordinates.y - (coordinates.x - (int(coordinates.x)&1)) / 2
     return Vector2(q, r)
     
-func axialToOddq(axialCoordinates):
+func axial_to_oddq(axialCoordinates):
     var q = axialCoordinates.x
     var r = axialCoordinates.y
     
     var x = q
-    var y = r + (q - (q&1)) / 2
+    var y = r + (q - (int(q)&1)) / 2
     
     return Vector2(x,y)
     
+func cube_round(frac):
+    var q = round(frac.x)
+    var r = round(frac.y)
+    var s = round(frac.z)
+
+    var q_diff = abs(q - frac.x)
+    var r_diff = abs(r - frac.y)
+    var s_diff = abs(s - frac.z)
+
+    if q_diff > r_diff and q_diff > s_diff:
+        q = -r-s
+    elif r_diff > s_diff:
+        r = -q-s
+    else:
+        s = -q-r
+
+    return Vector3(q, r, s)
+    
+func axial_round(hex):
+    return cube_to_axial(cube_round(axial_to_cube(hex)))
+
+func cube_to_axial(cube):
+    var q = cube.x
+    var r = cube.y
+    return Vector2(q, r)
+
+func axial_to_cube(hex):
+    var q = hex.x
+    var r = hex.y
+    var s = -q-r
+    return Vector3(q, r, s)
--- Hex.gd
+++ Hex.gd
@@ -1,18 +1,22 @@
 extends Line2D
 
 var size = 400
+
 var coordinates: Vector2
+var axialCoordinates: Vector2
 
 onready var bounds = $Bounds
 onready var coordinatesLabel = $Bounds/CoordinatesLabel
+onready var axialCoordinatesLabel = $Bounds/AxialCoordinatesLabel
 
 var debug := true
+var isHighlighted := false
 
 func _init():
     width = 1.0
     default_color = Color(0.4,0.4,0.4)
     
-func _ready():
+func _ready():   
     var center = Vector2(size, (sqrt(3) * size) / 2)
     bounds.rect_min_size = Vector2(size * 2, sqrt(3) * size)
     
@@ -20,7 +24,13 @@ func _ready():
     coordinatesLabel.text = coordinatesText
     coordinatesLabel.rect_position = Vector2(
         center.x - coordinatesLabel.rect_size.x / 2,
-        center.y - coordinatesLabel.rect_size.y / 2
+        center.y - coordinatesLabel.rect_size.y
+    )
+    var axialCoordinatesText = "%d,%d" % [axialCoordinates.x, axialCoordinates.y]
+    axialCoordinatesLabel.text = axialCoordinatesText    
+    axialCoordinatesLabel.rect_position = Vector2(
+        center.x - axialCoordinatesLabel.rect_size.x / 2,
+        center.y
     )
     
     set_points(
@@ -37,6 +47,16 @@ func _ready():
     
     #bounds.editor_only = !debug
     coordinatesLabel.visible = debug
+    
+    update()
+    
+func update():
+    if isHighlighted:
+        z_index = 1
+        default_color = Color(0.9,0.9,0.9)
+    else:
+        z_index = 0
+        default_color = Color(0.4,0.4,0.4)
           
 # From https://www.redblobgames.com/grids/hexagons/
 func corner(center, i):
